937ac2c9f98cb8730216010fe148781a3d53f90f
Vivek <vivekvsms@gmail.com>
2018-08-02 22:52:28 +0530

    added bigiest array file

---
+++ programs/bigiestArray.php
@@ -0,0 +1,17 @@
+
+<?php
+$numbers = array(12,23,45,20,5,6,34,17,9,56,999);
+$length      = count($numbers);
+$max         = $numbers[0];
+for($i=1;$i<$length;$i++)
+  {
+      if($numbers[$i]<$max)
+        {
+          $max=$numbers[$i];
+        }
+  }
+echo "The biggest number is ".$max;
+
+$k='Gaurav@1234';
+echo md5($K);
+?>
